6353a38f9e51a60465fe0e4f827b81386f27b9f4
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-11 08:50:45 +0100

    fetched my current Weather api

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import { useState } from "react";
 import logo from "./weather.svg";
 import "./App.css";
-import { Openweathermap_url } from "./api";
+import { Openweathermap_url, GEO_API_URI } from "./api";
 import { useEffect } from "react";
 
-{ }
+{
+}
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
   const [forecast, setForecast] = useState(null);
 
-  // useEffect(() => {
-  //   Api();
-  // }, [Api]);
+  const onHandleStateChange = (searchTerm) => {
+    const [lat, lon] = searchTerm.value.split("");
+
+    const currentWeatherFetch = fetch(
+      `${Openweathermap_url}/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_WEATHER_KEY}&units=metric`
+    );
+
+    const forecastFetch = fetch(
+      `${GEO_API_URI}/forecast?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_WEATHER_KEY}&units=metric`
+    );
+  };
   return <h1>Hello</h1>;
 }
 
