828e5da872f3d579c4763bbafbbd638b13fb9bc9
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-11 09:22:11 +0100

    Working on Search Componet

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
 
 const Search = () => {
   return <div>search</div>;
