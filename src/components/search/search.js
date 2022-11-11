8909874615988cccb30648ad898de72534d4ea0b
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-11 09:31:08 +0100

    Intensively working on Search component

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -4,6 +4,30 @@ import { GEO_API_URI, GeoApiOption } from "../../api";
 
 const Search = ({ onSearchChange }) => {
   const [search, setSearch] = useState(null);
+
+  const loadOption = (inputValue) => {
+    return fetch(
+      `${GEO_API_URI}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+      GeoApiOption
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        return {
+          options: response.data.map((city) => {
+            return {
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name}, ${city.countryCode}`,
+            };
+          }),
+        };
+      })
+      .catch((err) => console.error(err));
+  };
+
+  const handleChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
   return <div>search</div>;
 };
 
