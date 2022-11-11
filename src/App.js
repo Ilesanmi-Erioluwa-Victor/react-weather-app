7d0fa8c8d95f6ef724c9ea9127b3b9e724ffffb3
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-11 05:27:31 +0100

    Started working on Aoo.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
+import { useState } from "react";
 import logo from "./weather.svg";
 import "./App.css";
-import { Api } from "./api";
+import { Openweathermap_url, } from "./api";
 import { useEffect } from "react";
 
 function App() {
+  const [ currentWeather, setCurrentWeather ] = useState(null);
+   
   // useEffect(() => {
   //   Api();
   // }, [Api]);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <h1>Hello</h1>;
 }
 
 export default App;
