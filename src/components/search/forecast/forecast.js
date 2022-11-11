e2e58305e29cf7a1078461491c0a761d06fec313
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-11 09:44:32 +0100

    Working on Forecast

--- src/components/search/forecast/forecast.js
+++ src/components/search/forecast/forecast.js
@@ -1,6 +1,27 @@
 import React from "react";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemButton,
+  AccordionItemHeading,
+  AccordionItemPanel,
+} from "react-accessible-accordion";
+import "./forecast.css";
+
+const WEEKS_DAYS = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
 
 const ForeCast = () => {
+
+  const daysInaWeek = new Date().getDay();
+  
   return <div>Forest</div>;
 };
 
